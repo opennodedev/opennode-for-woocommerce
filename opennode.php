63d2aed82d492a4b4d7601a5e51545d3286a2c06
João Almeida <contact@joaoalmeida.me>
2023-07-19 09:27:11 -0500

    Fix tested up to wordpress version

--- opennode.php
+++ opennode.php
@@ -11,7 +11,7 @@ Author URI: https://opennode.com/about
 
 add_action('plugins_loaded', 'opennode_init');
 
-define('OPENNODE_WOOCOMMERCE_VERSION', '1.5.2');
+define('OPENNODE_WOOCOMMERCE_VERSION', '1.5.3');
 define('OPENNODE_CHECKOUT_PATH', 'https://checkout.opennode.com/');
 
 function opennode_init()
